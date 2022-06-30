0f7b602ff68de04d45115e4f4c827893be18ef41
Maksym_Sharov <sharom01@kits.kfplc.com>
2022-06-30 18:05:07 +0300

    Task 06. Add redux and MovieAPI as backend

---
+++ src/features/movies/moviesSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const movieSlice = createSlice({
+  name: 'counter',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload
+    },
+  },
+})
